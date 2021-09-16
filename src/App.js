5f4d73515d53f5b5ec4e391df2a46a9086481156
LeandroMenoci <menoci.leandro@gmail.com>
2021-09-15 23:07:59 -0300

    Componentizando o sistema
    Como podemos criar class components
    
    Entendendo como o JSX renderiza objetos
    
    Hierarquia entre componentes (Pai/filhos)
    
    Componentes podem ter filhos que são componentes
    
    Como integrar o Javascript no meio do seu JSX

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import FormularioCadastros from './components/formularioCadastro';
+import ListaDeNotas from './components/listaDeNotas';
+
+function App() {
+  return (
+    <section>
+      <FormularioCadastros />
+      <ListaDeNotas />
+    </section >
+  );
+}
+//react -> lib facebook
+//React -> ecossistema
+export default App;
---
+++ src/components/listaDeNotas.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react"
+import CardNota from "./CardNota"
+
+class ListaDeNotas extends React.Component {
+  render() {
+    return (
+      <ul>
+        {Array.of("Trabalha", "Trabalho", "Estudos").map((categoria) => {
+          return (
+            <li>
+              <div>{categoria}</div>
+              <CardNota />
+            </li>
+          )
+        })}
+      </ul>
+    );
+  }
+}
+
+export default ListaDeNotas;
