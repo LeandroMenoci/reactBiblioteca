c32f54b7345b31b3044377b5a6f1d736b910ac61
LeandroMenoci <menoci.leandro@gmail.com>
2021-09-26 22:13:14 -0300

    Ciclo de vida dos componentes, ComponentDidMount e ComponentWillUnmount

--- src/dados/Categorias.js
+++ src/dados/Categorias.js
@@ -5,15 +5,22 @@ export default class Categorias {
   }
 
   inscrever(func) {
-    this.inscritos.push(func);
+    this._inscritos.push(func);
+  }
+
+  desinscrever(func) {
+    this._inscritos = this._inscritos.filter(f => f !== func)
   }
 
   notificar() {
-    this._inscritos.forEach(func => { func(this.categorias) })
+    this._inscritos.forEach(func => {
+      func(this.categorias);
+    })
   }
 
   adicionarCategoria(novaCategoria) {
-    this.categorias.push(novaCategoria)
+    this.categorias.push(novaCategoria);
+    this.notificar()
   }
 }
 
